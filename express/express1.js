01bec3e18560d54b0d2d30e4a0e9d918c121a7e3
caipeng <caipeng@meidaifu.com>
2019-07-25 17:43:13 +0800

    1

--- express/express1.js
+++ express/express1.js
@@ -5,11 +5,16 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
  
-var server = app.listen(8081, function () {
+// var server = app.listen(8081, function () {
  
-  var host = server.address().address
-  var port = server.address().port
+//   var host = server.address().address
+//   var port = server.address().port
  
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+//   console.log("应用实例，访问地址为 http://%s:%s", host, port)
  
+// })
+
+app.listen(3000,()=>{
+  console.log('server start');
+  
 })
